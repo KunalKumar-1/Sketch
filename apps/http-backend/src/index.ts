2a4418902d15966b9e72d1e8d2fdd20c8abae773
KUNAL KUMAR <kunaldevspro@gmail.com>
2025-02-12 17:29:11 +0530

    http-bc: added signup end point
    Signed-off-by: KUNAL KUMAR <kunaldevspro@gmail.com>

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,28 +3,41 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
 import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
 
 const app = express();
+ app.use(express.json());
 
 
-app.post('/signup', (req, res) => {
+app.post('/signup', async (req, res) => {
  // db call
  // to check if the data user sending is correct or not 
 
- const data = CreateUserSchema.safeParse(req.body);
-
- if(!data.success){
+ const parseData = CreateUserSchema.safeParse(req.body);
+ if(!parseData.success){
     res.json({
         message: "Incorrect inputs"
     })
     return;
  }
-
- res.json({
-    userId: 123,
-    message: "User created"
- })
-
+    try{
+      const user =  await prismaClient.user.create({
+            data: {
+                email: parseData.data?.username,
+                //hash the password later
+                password: parseData.data.password,
+                name: parseData.data.name
+            }
+        })
+     res.json({
+        userId: user.id,
+        message: "User Created !"
+     })
+    } catch(e){
+        res.status(411).json({
+            message: "This username already exists"
+        })
+    }
 })
 
 
